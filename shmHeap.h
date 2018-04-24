ef3df5d7205bfe071c818e62c3b5f7d95f67fa8f
jriggs <Justin.Riggs@gemalto.com>
2018-04-24 17:01:13 -0400

    Initial version.  Works at least a little bit.

---
+++ shmHeap.h
@@ -0,0 +1,8 @@
+#ifndef __SHM_HEAP_H__
+#define __SHM_HEAP_H__
+
+extern void shmHeapInit(unsigned char *heap, size_t size);
+extern void *shmHeapMalloc(size_t size);
+extern void shmHeapFree(void *ptr);
+
+#endif // __SHM_HEAH_H__
